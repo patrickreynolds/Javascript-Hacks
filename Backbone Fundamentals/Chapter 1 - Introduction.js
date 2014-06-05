2a2b74a27a5238a7d536fa1d88c41765f2fc4586
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-06-05 01:17:56 -0700

    Create chapter files.

---
+++ Backbone Fundamentals/Chapter 1 - Introduction.js
@@ -0,0 +1,101 @@
+// Introduction to Backbone
+
+/*
+What does a js framework do?
+Client applicaitons pull raw data from the server and render it into
+the browser when and where it is needed.
+
+What is a use case for syncing up client and server side data?
+Take the one page shopping cart example. As ajax requests are
+getting called with jQuery, this introduces implicit model 
+data on the client side.
+*/
+
+
+/*
+Introduction:
+"You can't make an architect. You can however open the doors and windows 
+toward the light as you see it." - Frank Lloyd Wright
+
+Backbone solves this problem for you, providing a way to cleanly organize 
+code, separating responsibilities into recognizable pieces that are easy to maintain.
+*/
+
+/*
+What is MVC?
+
+MVC - Model View Controller
+
+Models:
+Models represent the domin-specific knowledge and data in an application. This 
+of this as being a 'type' of data you can model.
+Examples: User, Photo, or Todo note. Models can notify observers when their state changes.
+
+Views:
+Views typically constitute the user interface in an applicaiton (eg. markup and templates), 
+but don't have to be. They observe Models, but don't directly communicate with them.
+
+Controllers:
+Controllers handle input (e.g. clicks, user actions) and update Models
+
+In summary, user input is acted upon by Controller which update Models. Views 
+observe Models and update the user interface when changes occur.
+
+*/
+
+/*
+What is Backbone.js?
+Backbone.js is a lightweight JavaScript library that adds structure to your client-side 
+code. It makes it easy to manage and decouple concerns in your application, leaving you 
+with code that is more maintainable in the long term.
+
+Backbone is commonly used for Single Page Applications (SPAs). SPAs are web applications
+that load into the browser and then react to data changes on the client side without 
+requiring comple page refreshes from the server
+
+*/
+
+/*
+When Do I Need A JavaScropt MVC Framework?
+Common use cases for using a javascript framework is to accommodate a complex user interface 
+or simple trying to reduce the nubmer of HTTP requests required for new Views
+	
+*/
+
+/*
+Why Consider Backbone.js?
+
+Backbone provides a minimal set of data-structuring (Models, Collections) and user interface 
+(Views, URLs) primitives that are helpful when building dymamic applications using JavaScript.
+
+*/
+
+/*
+Setting Expectations
+
+This section talks about the various chapters that are talked about in this book
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
